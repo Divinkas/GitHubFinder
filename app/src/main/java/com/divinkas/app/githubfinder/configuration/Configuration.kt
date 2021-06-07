baf5f9b50477289e70588fc916c52f68556ba83a
yatsenko <a.yatsenko@mycredit.ua>
2021-06-07 18:27:26 +0300

    Created new variable

--- app/src/main/java/com/divinkas/app/githubfinder/configuration/Configuration.kt
+++ app/src/main/java/com/divinkas/app/githubfinder/configuration/Configuration.kt
@@ -7,6 +7,7 @@ object Configuration {
     const val LIMIT_ON_PAGE = 30
     private const val LIMIT_RESULTS_COUNT = 15
     const val STEP_PAGINATION = 2
+    const val STEP_PAGINATION_NEXT = 3
 
     fun getConfigUtils() = ConfigUtils(LINK_API, LIMIT_RESULTS_COUNT)
 }
