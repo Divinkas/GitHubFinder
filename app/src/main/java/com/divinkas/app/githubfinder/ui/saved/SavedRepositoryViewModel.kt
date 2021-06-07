8b8bd9b00f07a4466369c894f6333279fdad6bcf
yatsenko <a.yatsenko@mycredit.ua>
2021-06-07 18:28:50 +0300

    Created onCreate method

--- app/src/main/java/com/divinkas/app/githubfinder/ui/saved/SavedRepositoryViewModel.kt
+++ app/src/main/java/com/divinkas/app/githubfinder/ui/saved/SavedRepositoryViewModel.kt
@@ -1,6 +1,10 @@
 package com.divinkas.app.githubfinder.ui.saved
 
 import com.divinkas.app.githubfinder.ui.GitHubViewModel
+import timber.log.Timber
 
 class SavedRepositoryViewModel : GitHubViewModel() {
+    fun onCreate() {
+        Timber.i("onCreate")
+    }
 }
